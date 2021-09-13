e971fd3b23317f60db918179114063b475d82159
Vladan Stanisic <vladanstanisic96@gmail.com>
2021-09-13 20:14:47 +0200

    Small modification

--- Tasks-Back/src/main/resources/data.sql
+++ Tasks-Back/src/main/resources/data.sql
@@ -2,9 +2,9 @@
 INSERT INTO `user` (id, username, password, role)
               VALUES (1,'miroslav','$2y$12$NH2KM2BJaBl.ik90Z1YqAOjoPgSd0ns/bF.7WedMxZ54OhWQNNnh6','ADMIN');
 INSERT INTO `user` (id, username, password, role)
-              VALUES (2,'tamara','$2y$12$DRhCpltZygkA7EZ2WeWIbewWBjLE0KYiUO.tHDUaJNMpsHxXEw9Ky','USER');
+              VALUES (2,'tamara','$2y$12$DRhCpltZygkA7EZ2WeWIbewWBjLE0KYiUO.tHDUaJNMpsHxXEw9Ky','KORISNIK');
 INSERT INTO `user` (id, username, password, role)
-              VALUES (3,'petar','$2y$12$i6/mU4w0HhG8RQRXHjNCa.tG2OwGSVXb0GYUnf8MZUdeadE4voHbC','USER');
+              VALUES (3,'petar','$2y$12$i6/mU4w0HhG8RQRXHjNCa.tG2OwGSVXb0GYUnf8MZUdeadE4voHbC','KORISNIK');
               
                             
 INSERT INTO state (id, name) VALUES (1, 'NEW');
